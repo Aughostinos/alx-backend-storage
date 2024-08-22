39a46fc4c7bc85d5f7e47fc05c75b53a47a565a8
Aughostinos <augustinos.nabil@gmail.com>
2024-08-22 20:24:30 +0300

    task 0

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+--  SQL script that creates a table users
+
+CREATE TABLE users if NOT EXISTS (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
