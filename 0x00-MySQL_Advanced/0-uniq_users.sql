d37ec38fda08d09f06d8945897bdf9a67f4e3e75
Aughostinos <augustinos.nabil@gmail.com>
2024-08-23 01:51:22 +0300

    fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
---  SQL script that creates a table users
+-- SQL script that creates a table users
 
-CREATE TABLE users if NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) 
