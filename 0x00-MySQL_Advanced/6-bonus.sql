900738977c3b5a1b1eed08c70a5d6a0b393d11eb
Aughostinos <augustinos.nabil@gmail.com>
2024-08-23 05:00:56 +0300

    task 6

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,21 @@
+-- SQL script that creates a stored procedure AddBonus
+-- that adds a new correction for a student.
+
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus(
+    IN user_id INT,
+    IN project_name VARCHAR(255),
+    IN score INT
+)
+BEGIN
+    INSERT INTO projects (name) 
+    VALUES (project_name) 
+    ON DUPLICATE KEY UPDATE id = LAST_INSERT_ID(id);
+    
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, LAST_INSERT_ID(), score);
+
+END$$
+
+DELIMITER ;
