c5471f2c2741adb184060f76d1d31f03d9d1d2af
Aughostinos <augustinos.nabil@gmail.com>
2024-08-28 02:49:17 +0300

    task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+10. Change school topics
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Python function that changes all topics of
+    a school document based on the name"""
+    update_doc = mongo_collection.updata_many(
+        { "name": name },
+        { "$set": {"topics": topics } }
+    )
