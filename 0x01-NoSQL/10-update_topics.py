b97e3d09b192c12c8b2fe04e3b3c6cd2c7be1a12
Aughostinos <augustinos.nabil@gmail.com>
2024-08-28 02:53:29 +0300

    fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,7 +7,7 @@
 def update_topics(mongo_collection, name, topics):
     """Python function that changes all topics of
     a school document based on the name"""
-    mongo_collection.updata_many(
+    mongo_collection.update_many(
         { "name": name },
         { "$set": {"topics": topics } }
     )
