527d2c72908ee452f48ce451df9d907be1491caf
Aughostinos <augustinos.nabil@gmail.com>
2024-08-28 01:59:23 +0300

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Task 8. List all documents in Python
+"""
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection."""
+    documents = list(mongo_collection.find())
+    return documents
