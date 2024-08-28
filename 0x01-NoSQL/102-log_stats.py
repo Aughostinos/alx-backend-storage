3ce7dcbcfa16e9b34a8fcd702e796d8ab63d3b0f
Aughostinos <augustinos.nabil@gmail.com>
2024-08-28 05:21:49 +0300

    task 15

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+task 15. Log stats - new version
+"""
+
+
+def log_stats():
+    """ provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('mongodb://localhost:27017/')
+    nginx_collection = client.logs.nginx
+
+    #first line: x logs where x is the number of documents
+    logs_num = nginx_collection.count_documents({})
+    print(f"{logs_num} logs")
+
+    # documents number by method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = nginx_collection.count_documents(
+            { "method": method }
+        )
+        print(f"\tmethod {method}: {method_count}")
+
+    #one line with the number of documents with method=GET path=/status
+    status_check = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print(f"{status_check} status check")
+
+    # top 10 Ips
+    print("IPs:")
+    ips = [
+        { "$group": {"_id": "$ip", "count": {"sum": 1 }}},
+        { "$sort": {"count": -1}},
+        {"$limit": 10}
+    ]
+    top_10_ips = nginx_collection.aggregate(ips)
+    for ip in ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    log_stats()
