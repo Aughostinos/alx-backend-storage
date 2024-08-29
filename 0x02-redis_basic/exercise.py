d8ac41341637d47c8aa9dfb3a9f7e49798cccdf6
Aughostinos <augustinos.nabil@gmail.com>
2024-08-29 03:04:21 +0300

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,8 +16,8 @@ class Cache:
         self._redis.flushdb()
 
 
-def store(self, data: Union[str, bytes, int, float]) -> str:
-    """The method should generate a random key """
-    key = str(uuid.uuid4())
-    self._redis.set(key, data)
-    return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """The method should generate a random key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
