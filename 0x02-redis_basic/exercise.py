20c5a1a5cb2110ffa0d8bacb6fced1003b1b9228
Aughostinos <augustinos.nabil@gmail.com>
2024-08-29 03:03:11 +0300

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Task 0. Writing strings to Redis"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """cache class to store data in redis"""
+
+
+    def __init__(self):
+        """store an instance of the Redis client
+        as a private variable named _redis"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+
+def store(self, data: Union[str, bytes, int, float]) -> str:
+    """The method should generate a random key """
+    key = str(uuid.uuid4())
+    self._redis.set(key, data)
+    return key
